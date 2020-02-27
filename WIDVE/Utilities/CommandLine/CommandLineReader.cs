4354ea7ecb490db21fe3e6ba4a8f262df1c642c1
Simon <spsmith5@wisc.edu>
2020-02-27 14:24:21 -0600

    started on command line reader script

---
+++ WIDVE/Utilities/CommandLine/CommandLineReader.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WIDVE.Utilities
+{
+	[DefaultExecutionOrder(-1000)]
+	public class CommandLineReader : MonoBehaviour
+	{
+		
+	}
+}
