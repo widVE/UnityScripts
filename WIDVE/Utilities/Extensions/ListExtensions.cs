50c8c87474db08fd6e132da8846a1e06d2d9bb98
Simon <spsmith5@wisc.edu>
2020-03-13 14:45:42 -0500

    added list shuffle function

---
+++ WIDVE/Utilities/Extensions/ListExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace WIDVE.Utilities
+{
+	public static class ListExtensions
+	{
+		static System.Random RNG = new System.Random();
+
+		public static void Shuffle<T>(this IList<T> list)
+		{
+			//https://stackoverflow.com/questions/273313/randomize-a-listt
+			int i = list.Count;
+			while(i > 1)
+			{
+				i--;
+				int j = RNG.Next(i + 1);
+				T t = list[j];
+				list[j] = list[i];
+				list[i] = t;
+			}
+		}
+	}
+}
